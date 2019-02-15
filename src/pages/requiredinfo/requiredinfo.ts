ffcc72cefaeec4d14bf5d89ab595db5f533701ac
villagerboy <ankitshukla1992@outlook.com>
2019-02-15 16:02:30 +0530

    early riser && late sleeper

--- src/pages/requiredinfo/requiredinfo.ts
+++ src/pages/requiredinfo/requiredinfo.ts
@@ -10,29 +10,31 @@ import 'rxjs/add/operator/map'
   templateUrl: 'requiredinfo.html',
 })
 export class RequiredinfoPage {
-  datewisedata:any=[];
+  datewisedata: any = [];
   info = {
     title: 'Chat Info',
     usemsg: '',
     group: [],
-    groupcreatedday:'',
+    groupcreatedday: '',
     userlist: [],
-    mostmessager:{},
-    leastmesseager:{},
+    mostmessager: {},
+    leastmesseager: {},
     usermsgcount: [],
     totalmsg: '',
     totaluser: '',
-    mostmessagerOfday:{},
-    totalletter:'',
-    totalwords:'',
-    mediacount:0,
-    usermedia:'',
-    mostmediabyuser:'',
-    mostmsgdate:'',
-    leastmsgdate:'',
-    datewisemsgcount:[],
-    datewiseusermessage:[],
-    mostwordused:[]
+    mostmessagerOfday: {},
+    totalletter: '',
+    totalwords: '',
+    mediacount: 0,
+    usermedia: '',
+    mostmediabyuser: '',
+    mostmsgdate: '',
+    leastmsgdate: '',
+    datewisemsgcount: [],
+    datewiseusermessage: [],
+    mostwordused: [],
+    earlyriser: '',
+    lateriser: ''
   }
   constructor(public http: Http, public navCtrl: NavController, public navParams: NavParams) {
     this.getLocalData();
@@ -48,161 +50,205 @@ export class RequiredinfoPage {
 
       getrowfromstring.forEach(element => {
         let getdatefromrow = element.split('-');
-        let dateflow=getdatefromrow[0].split(',');
+        let dateflow = getdatefromrow[0].split(',');
         if (getdatefromrow[1] !== undefined) {
           let splituser_msg = getdatefromrow[1].split(':');
           let db = { date: getdatefromrow[0], user: splituser_msg[0], message: splituser_msg[1] };
-          let dbdate= { date: dateflow[0],time:dateflow[1], user: splituser_msg[0], message: splituser_msg[1] };
+          let dbdate = { date: dateflow[0], time: dateflow[1], user: splituser_msg[0], message: splituser_msg[1] };
           this.datewisedata.push(dbdate);
           datas.push(db);
         }
 
       });
+
+      /* Filter Record between Two hour BEGIN  */
+      this.datewisedata = this.datewisedata.filter(x => x.message !== undefined);
+      this.datewisedata.forEach(el => {
+        let dnew: any = el.date + ',' + el.time;
+        var hh = new Date(dnew);
+
+        el.hour = hh.getHours();
+      });
+
+      /** early Riser  */
+      let earlyriser: any = this.datewisedata.filter(x => x.hour >= "5" && x.hour <= "8");
+      let msgflowearly: any = new List<any>(earlyriser).GroupBy(y => y.user, x => x.message);
+      this.getearlyriser(msgflowearly, 'e');
+      /** Early riser END  */
+      /** Late Sleepr */
+      let latesleer: any = this.datewisedata.filter(x => x.hour >= "0" && x.hour <= "4");
+      let msgflowlate: any = new List<any>(latesleer).GroupBy(y => y.user, x => x.message);
+
+      this.getearlyriser(msgflowlate, 'l');
+      /** Late Sleepr */
+      /** Filter records between two hour END */
+
+
       let db: any = datas.filter(x => x.message !== undefined);
-      this.datewisedata= this.datewisedata.filter(x => x.message !== undefined)
-   //   console.table(this.datewisedata);
-      // console.table(db);
+      this.datewisedata = this.datewisedata.filter(x => x.message !== undefined)
+
       this.info.group = db;
-      let leastdate= new List<any>(db).OrderBy(x=>x.date).First();
-      // console.table(leastdate);
+      let leastdate = new List<any>(db).OrderBy(x => x.date).First();
+
       var date1 = new Date(leastdate.date);
-var date2 = new Date();
-var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24))+1; 
-this.info.groupcreatedday=diffDays.toString();
-this.getmessagecount(db);
+      var date2 = new Date();
+      var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+      var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)) + 1;
+      this.info.groupcreatedday = diffDays.toString();
+      this.getmessagecount(db);
     });
   }
   getmessagecount = (db: any) => {
 
     let groups = new List<group>(db);
-    let groupsdate = new List<group>( this.datewisedata);
+    let groupsdate = new List<group>(this.datewisedata);
     //  this.datewisedata
-    let userlist:any = groups.Select(x => x.user).Distinct().ToArray();
-    this.info.userlist=userlist; 
-    let count = groups.Select(x => x.user).Distinct().Count();    
+    let userlist: any = groups.Select(x => x.user).Distinct().ToArray();
+    this.info.userlist = userlist;
+    let count = groups.Select(x => x.user).Distinct().Count();
     let users = groups.Select(x => x.user).Distinct().ToArray();
     let msgcount = groups.Select(x => x.message).Count();
     this.info.totaluser = `Total user in group ${count}`;
     this.info.totalmsg = `Total  messages are ${msgcount} `;
     // console.log(`total count of message in group ${msgcount}`);
-    
+
     let msgflow: any = groups.GroupBy(y => y.user, x => x.message);
     let msgflowbydate: any = groupsdate.GroupBy(y => y.date, x => x.message);
-    let msgflowbydateuser: any = groupsdate.GroupBy(y => y.date,x=>x);
-   // debugger;
-//console.log('userwisemsg'+msgflowbydateuser.toString());
+    let msgflowbydateuser: any = groupsdate.GroupBy(y => y.date, x => x);
+    // debugger;
+    //console.log('userwisemsg'+msgflowbydateuser.toString());
     /** print chart Data  */
     this.generategroupchartdata(msgflow);
     this.generategroupchartdatabydate(msgflowbydate);
     this.generateuserdatabydate(msgflowbydateuser);
-   //
+    //
     /** print chart data end  */
   }
   generategroupchartdata(db: any) {
-    debugger;
+
     let Groupcircle: any = [];
-    let Groupcirclemedia :any=[];
-    let stringlength:any='';
-    let meadia:any=0;
+    let Groupcirclemedia: any = [];
+    let stringlength: any = '';
+    let meadia: any = 0;
     for (var key in db) {
       if (db.hasOwnProperty(key)) {
         var s = '';
         for (let ss in db[key]) {
           s += db[key][ss];
-         meadia= s.split( new RegExp( "<Media omitted>", "gi" ) ).length-1;
-          stringlength+=db[key][ss];
+          meadia = s.split(new RegExp("<Media omitted>", "gi")).length - 1;
+          stringlength += db[key][ss];
         }
         let info = { user: key, totalmsg: db[key].length, msgstring: s }
-        let meadiainfo={ user: key, totalmsg: db[key].length, msgstring: meadia }
+        let meadiainfo = { user: key, totalmsg: db[key].length, msgstring: meadia }
         Groupcircle.push(info);
         Groupcirclemedia.push(meadiainfo);
-     
+
       }
-      
-    
+
+
     }
     //  word count success debugger;
-    let dbs:any = this.removeWord(stringlength,'<Media omitted>');//stringlength.replace(/<Media omitted>/g,' ');
-    let a:any =this.nthMostCommon(dbs,6);
-    
-    let emojicount=this.fancyCount(stringlength);
-    console.log('total emoji'+emojicount);
-      this.info.totalletter=stringlength.length;      
-      this.info.totalwords=this.countWords(stringlength);
-      this.info.mediacount=stringlength.split( new RegExp( "<Media omitted>", "gi" ) ).length-1;
+    let dbs: any = this.removeWord(stringlength, '<Media omitted>');//stringlength.replace(/<Media omitted>/g,' ');
+    let a: any = this.nthMostCommon(dbs, 6);
+
+    let emojicount = this.fancyCount(stringlength);
+    console.log('total emoji' + emojicount);
+    this.info.totalletter = stringlength.length;
+    this.info.totalwords = this.countWords(stringlength);
+    this.info.mediacount = stringlength.split(new RegExp("<Media omitted>", "gi")).length - 1;
     //  console.log('total words'+ this.info.totalwords);   
-        this.info.usermsgcount = Groupcircle;
-        this.info.usermedia=Groupcirclemedia;
-        let mostmediabyuser:any = new List<any>( Groupcirclemedia).OrderByDescending(x=>x.msgstring).First();
-        this.info.mostmediabyuser=mostmediabyuser.user;
-      //  console.table('mostmediabyuser'+mostmediabyuser.user);
-    let mostmessager = new List<any>(Groupcircle).OrderByDescending(x=>x.totalmsg).First();
-    let leastmessager = new List<any>(Groupcircle).OrderByDescending(x=>x.totalmsg).Last();
-    this.info.mostmessager=mostmessager;
-    this.info.leastmesseager=leastmessager;
+    this.info.usermsgcount = Groupcircle;
+    this.info.usermedia = Groupcirclemedia;
+    let mostmediabyuser: any = new List<any>(Groupcirclemedia).OrderByDescending(x => x.msgstring).First();
+    this.info.mostmediabyuser = mostmediabyuser.user;
+    //  console.table('mostmediabyuser'+mostmediabyuser.user);
+    let mostmessager = new List<any>(Groupcircle).OrderByDescending(x => x.totalmsg).First();
+    let leastmessager = new List<any>(Groupcircle).OrderByDescending(x => x.totalmsg).Last();
+    this.info.mostmessager = mostmessager;
+    this.info.leastmesseager = leastmessager;
     // this.generategroupchartdatabydate(usermedia);
 
   }
- removeWord = (a:any,searchWord)=>{
+  removeWord = (a: any, searchWord) => {
     var str = a;
     var n = str.search(searchWord);
-    while(str.search(searchWord) > -1){
-        n = str.search(searchWord);
-        str = str.substring(0, n) + str.substring(n + searchWord.length, str.length);
+    while (str.search(searchWord) > -1) {
+      n = str.search(searchWord);
+      str = str.substring(0, n) + str.substring(n + searchWord.length, str.length);
     }
     return str;
-}
-  generategroupchartdatabydate(db:any){
-    let Groupcircle: any = [];    
-    for (var key in db) {
-      if (db.hasOwnProperty(key)) {       
-        let info = { date: key, totalmsg: db[key].length }     
-        Groupcircle.push(info);
-      }
   }
- 
-  
-  new List<any>(Groupcircle).OrderBy(x=>new Date(x.date.toString('dd-MMM-yyyy')));
-  
- 
-  this.info.datewisemsgcount=Groupcircle;
- // console.table(this.info.datewisemsgcount);
-  let mostmessagerdate = new List<any>(Groupcircle).OrderBy(x=>new Date(x.date.toString('dd-MMM-yyyy'))).OrderByDescending(x=>x.totalmsg).First();
-  let leastmessagerdate = new List<any>(Groupcircle).OrderBy(x=>new Date(x.date.toString('dd-MMM-yyyy'))).OrderByDescending(x=>x.totalmsg).Last();
-  this.info.mostmsgdate=mostmessagerdate;
- this.info.leastmsgdate=leastmessagerdate;
-}
-generateuserdatabydate(db:any){
-  let Groupcircle: any = [];    
-  for (var key in db) {
-   
-    if (db.hasOwnProperty(key)) {      
-      let userinfodate=new List<any>(db[key]).GroupBy(y => y.user,x=>x.message); 
-      for (var keys in userinfodate) {
-        if (userinfodate.hasOwnProperty(keys)) {
-        let info = { date: key, user:keys,totalmesage:userinfodate[keys].length }     
-        Groupcircle.push(info);
-        }
-      }
-     
-    }
-    this.info.datewiseusermessage=Groupcircle;
-  //  console.table(Groupcircle);
-}
-}
+  getearlyriser = (db: any, type: string) => {
 
-  countWords=(s)=>{
-    s = s.replace(/(^\s*)|(\s*$)/gi,"");//exclude  start and end white-space
-    s = s.replace(/[ ]{2,}/gi," ");//2 or more space to 1
-    s = s.replace(/\n /,"\n"); // exclude newline with a start spacing
-    return s.split(' ').filter(function(str){return str!="";}).length;
-    
-}
- fancyCount(str){
-  return /[^\u0000-\u00ff]/.test(str);
- // return Array.from(str.split(/[\ufe00-\ufe0f]/).join("")).length;
-}
+    let Groupcircle: any = [];
+    for (var key in db) {
+      if (db.hasOwnProperty(key)) {
+        let info = { user: key, totalmsg: db[key].length }
+        Groupcircle.push(info);
+      }
+    }
+
+    if (type === 'e') {
+      let earlyrisername: any = new List<any>(Groupcircle).OrderByDescending(x => x.totalmsg).First();
+      this.info.earlyriser = earlyrisername.user;
+      console.table(earlyrisername.user)
+    } else if (type === 'l') {
+      let latesleer: any = new List<any>(Groupcircle).OrderByDescending(x => x.totalmsg).First();
+      this.info.lateriser = latesleer.user;
+      console.table(latesleer.user);
+    }
+
+  }
+  generategroupchartdatabydate(db: any) {
+    let Groupcircle: any = [];
+    for (var key in db) {
+      if (db.hasOwnProperty(key)) {
+        let info = { date: key, totalmsg: db[key].length }
+        Groupcircle.push(info);
+      }
+    }
+
+
+    new List<any>(Groupcircle).OrderBy(x => new Date(x.date.toString('dd-MMM-yyyy')));
+
+
+    this.info.datewisemsgcount = Groupcircle;
+    // console.table(this.info.datewisemsgcount);
+    let mostmessagerdate = new List<any>(Groupcircle).OrderBy(x => new Date(x.date.toString('dd-MMM-yyyy'))).OrderByDescending(x => x.totalmsg).First();
+    let leastmessagerdate = new List<any>(Groupcircle).OrderBy(x => new Date(x.date.toString('dd-MMM-yyyy'))).OrderByDescending(x => x.totalmsg).Last();
+    this.info.mostmsgdate = mostmessagerdate;
+    this.info.leastmsgdate = leastmessagerdate;
+  }
+  generateuserdatabydate(db: any) {
+    let Groupcircle: any = [];
+    for (var key in db) {
+
+      if (db.hasOwnProperty(key)) {
+        let userinfodate = new List<any>(db[key]).GroupBy(y => y.user, x => x.message);
+        for (var keys in userinfodate) {
+          if (userinfodate.hasOwnProperty(keys)) {
+            let info = { date: key, user: keys, totalmesage: userinfodate[keys].length }
+            Groupcircle.push(info);
+          }
+        }
+
+      }
+      this.info.datewiseusermessage = Groupcircle;
+      //  console.table(Groupcircle);
+    }
+  }
+
+  countWords = (s) => {
+    s = s.replace(/(^\s*)|(\s*$)/gi, "");//exclude  start and end white-space
+    s = s.replace(/[ ]{2,}/gi, " ");//2 or more space to 1
+    s = s.replace(/\n /, "\n"); // exclude newline with a start spacing
+    return s.split(' ').filter(function (str) { return str != ""; }).length;
+
+  }
+  fancyCount(str) {
+    return /[^\u0000-\u00ff]/.test(str);
+    // return Array.from(str.split(/[\ufe00-\ufe0f]/).join("")).length;
+  }
   /** find most  common world used Begin */
   nthMostCommon(string, ammount) {
     var wordsArray = string.split(/\s/);
